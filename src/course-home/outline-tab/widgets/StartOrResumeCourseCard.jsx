fb76db3bbcdc488c8c875f62a57dbffbf3007fad
Arpit-Nakrani-Networked <arpit.nakrani@networked.co>
2025-07-16 14:20:13 +0200

    FIX | removed extra words in title

--- src/course-home/outline-tab/widgets/StartOrResumeCourseCard.jsx
+++ src/course-home/outline-tab/widgets/StartOrResumeCourseCard.jsx
@@ -50,7 +50,7 @@ const StartOrResumeCourseCard = ({ intl }) => {
 
   return (
     <Card className="mb-3 raised-card card p-4 d-flex flex-row justify-content-between align-items-center" data-testid="start-resume-card">
-      <h2 className="card-header-custom mr-3 mb-0 welcome-text">{hasVisitedCourse ? `Welcome, John - ${intl.formatMessage(messages.resumeBlurb)}` : 'Welcome, John'}</h2>
+      <h2 className="card-header-custom mr-3 mb-0 welcome-text">Welcome, John</h2>
       <Button
         variant="brand"
         className="btn-filled"
